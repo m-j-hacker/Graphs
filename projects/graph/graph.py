24ae328a61df8333fab44536546a1374f66f65b5
m-j-hacker <mjhacker@gmail.com>
2019-05-22 15:15:26 -0500

    Graphs funcs finished

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -12,18 +12,33 @@ class Graph:
         Add a vertex to the graph.
         """
         
-        pass  # TODO
+        # First check to see if vertex is in vertices
+        if vertex not in self.vertices:
+            # If not in vertices, add it
+            self.vertices[vertex] = []
     def add_edge(self, v1, v2):
         """
         Add a directed edge to the graph.
         """
-        pass  # TODO
+        # First check if edge is present
+        if v1 in self.vertices:
+            self.vertices[v1].append[v2]
+        else:
+            self.vertices[v1] = [v2]
     def bft(self, starting_vertex):
         """
         Print each vertex in breadth-first order
         beginning from starting_vertex.
         """
-        pass  # TODO
+        q = Queue()
+        q.enqueue([starting_vertex])
+        visisted = set()
+        while q:
+            vertex = q.pop(0)
+            if vertex not in visisted:
+                visisted.add(vertex)
+                q.extend(self.vertices[vertex] - visited)
+
     def dft(self, starting_vertex):
         """
         Print each vertex in depth-first order
@@ -41,6 +56,7 @@ class Graph:
             vert = s.pop()
             # If not in visited, mark it as visited
             if vert not in visited:
+                print (vert)
                 # TODO - finish add_vertex def!
                 # visited.add(vert)
                 # And then add all neighbors to stack
@@ -52,21 +68,66 @@ class Graph:
         beginning from starting_vertex.
         This should be done using recursion.
         """
-        pass  # TODO
+        # We will use stack here for dft
+        visited, stack = set(), [starting_vertex]
+        while stack:
+            vertex = stack.pop()
+            if vertex not in visited:
+                # visited.add(vertex)
+                print(vertex)
+                stack.extend(self.vertices[vertex] - visited)
     def bfs(self, starting_vertex, destination_vertex):
         """
         Return a list containing the shortest path from
         starting_vertex to destination_vertex in
         breath-first order.
         """
-        pass  # TODO
+        # Here we will use a queue
+        q = Queue()
+        # Load up the starting vertex
+        q.enqueue([starting_vertex])
+        #Initialize the visisted set
+        visited = set()
+        # Run a loop as long as q contains anything
+        while q.size() > 0:
+            path = q.dequeue()
+            v = path[-1]
+            if v not in visited:
+                if v = destination_vertex:
+                    return path
+                visited.add(v)
+                for next_vert in self.vertices[v]:
+                    new_path = list(path)
+                    new_path.append(next_vert)
+                    q.dequeue(new_path)
+        return None
     def dfs(self, starting_vertex, destination_vertex):
         """
         Return a list containing a path from
         starting_vertex to destination_vertex in
         depth-first order.
         """
-        pass  # TODO
+        # We will use a stack here
+        s = Stack()
+        # Next, we will load up the starting vertex
+        s.push([starting_vertex])
+        # Initialize visisted
+        visisted = set()
+        # Run a loop as long as the stack contains anything
+        while s.size() > 0:
+            path = s.pop()
+            v = path[-1]
+            if v not in visisted:
+                if v == destination_vertex:
+                    return path
+                visited.add(v)
+                for next_vert in self.vertices[v]:
+                    new_path = list(path)
+                    new_path.append(next_vert)
+                    s.push(new_path)
+        return None
+
+        
 
 
 
